7e607c7044a5b48f4a51bed98b9026a6f2d2c472
Dhvanit41 <dhvanitpopat555@gmail.com>
2022-02-22 12:22:33 +0530

    removed deprecated reqest module and added axios module

---
+++ src/controllers/utils.js
@@ -0,0 +1,19 @@
+async function callSendAPI(sender_psid, response) {
+  try {
+    let request_body = {
+      recipient: {
+        id: sender_psid,
+      },
+      message: response,
+    };
+    let res = await axios({
+      uri: "https://graph.facebook.com/v6.0/me/messages",
+      qs: { access_token: process.env.FACEBOOK_PAGE_TOKEN },
+      method: "POST",
+      json: request_body,
+    });
+    return res;
+  } catch (e) {
+    throw new Error(e);
+  }
+}
