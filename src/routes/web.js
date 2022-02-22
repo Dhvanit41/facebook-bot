5491dabe8828284aca66adf31f7dde5181dc2b15
Dhvanit41 <dhvanitpopat555@gmail.com>
2022-02-22 15:03:00 +0530

    reverted code

--- src/routes/web.js
+++ src/routes/web.js
@@ -3,7 +3,10 @@ const {postWebHook,getWebHook} = require('../controllers/chatBotController')
 const router = express.Router()
 const homepageController = require('../controllers/homepageController')
 let initWebRoutes = (app)=>{
-   router.get('/',homepageController.getHomePage);
+  // router.get('/',homepageController.getHomePage);
+  router.get("/",(req,res)=>{
+      return res.send("hello world")
+  })
    router.get('/webHook',getWebHook);
    router.post('/webHook',postWebHook)
    return app.use('/',router);
